a536d1f6315002a00871702a8799b77458025be3
Lucas <lucasmorais582@gmail.com>
2020-07-27 22:18:23 -0300

    Estilizando página do repositório

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import { Title, Form, Repositories, Error } from './styles';
 import logoImg from '../../assets/logo.svg';
 import api from '../../services/api';
@@ -61,14 +62,14 @@ const Dashboard: React.FC = () => {
       {inputError && <Error>{inputError}</Error>}
       <Repositories>
         {repositories.map(repository => (
-          <a key={repository.full_name} href="teste">
+          <Link key={repository.full_name} to={`/repository/${repository.full_name}`}>
             <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
             <div>
               <strong>{repository.full_name}</strong>
               <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20}/>
-          </a>
+          </Link>
         ))}
 
       </Repositories>
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,61 @@
 import React from 'react';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { useRouteMatch, Link } from 'react-router-dom';
+import logoImg from '../../assets/logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <>
+    <Header>
+      <img src={ logoImg } alt="Github Explorer"/>
+      <Link to="/">
+        <FiChevronLeft size={16}/>
+        Voltar
+      </Link>
+    </Header>
+
+    <RepositoryInfo>
+      <header>
+        <img src="https://avatars1.githubusercontent.com/u/23035256?s=460&u=40a8e8342b00bef5657a18d2c4c551940becf897&v=4" alt="Lucas Lindo"/>
+          <div>
+            <strong>Lucas Lindo</strong>
+            <p>Descrição</p>
+        </div>
+      </header>
+      <ul>
+        <li>
+          <strong>1000</strong>
+          <span>Stars</span>
+        </li>
+        <li>
+          <strong>1000</strong>
+          <span>Forks</span>
+        </li>
+        <li>
+          <strong>1000</strong>
+          <span>Issues Abertas</span>
+        </li>
+      </ul>
+    </RepositoryInfo>
+
+    <Issues>
+      <Link key={} to={``}>
+        <div>
+          <strong>{}</strong>
+          <p>{}</p>
+        </div>
+        <FiChevronRight size={20}/>
+      </Link>
+    </Issues>
+    </>
+  );
 }
 
 export default Repository;
